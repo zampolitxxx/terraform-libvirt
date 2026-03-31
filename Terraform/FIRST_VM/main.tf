6b7ac9053d70ec6807f86832c039a58367f09373
Grishchenov Sergey <grischenov@ujin.tech>
2026-03-31 10:18:33 +0300

    first instance

---
+++ Terraform/FIRST_VM/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.6"
+    }
+  }
+}
+
+
+# Для удаленной машины:
+provider "libvirt" {
+  uri = "qemu+tcp://192.168.56.69/system"
+}
+
+
+# Пул хранения (если уже есть — пропусти)
+resource "libvirt_pool" "default" {
+  name = "default"
+  type = "dir"
+  path = "/var/lib/libvirt/images"
+}
+
+# Образ ОС
+resource "libvirt_volume" "debian" {
+  name = "debian-13-genericcloud-amd64.qcow2"
+  pool = libvirt_pool.default.name
+  source = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+  #source = "debian-13-genericcloud-amd64.qcow2"  # Локальный путь к тому
+  format = "qcow2"
+
+  depends_on = [libvirt_pool.default]
+}
+
+resource "libvirt_cloudinit_disk" "init" {
+  name = "cloud-init.iso"       # Имя облачного диска
+  user_data = file("cloud-init.yml")  # Путь к вашему файлу cloud-init
+}
+
+# VM
+resource "libvirt_domain" "vm1" {
+  name   = "vm1"
+  memory = 2048
+  vcpu   = 2
+
+  disk {
+    volume_id = libvirt_volume.debian.id
+  }
+
+  network_interface {
+    network_name = "default"
+    mac = "52:54:00:12:34:56"  # MAC для DHCP резервирования
+  }
+  
+  cloudinit = libvirt_cloudinit_disk.init.id  # Подключаем cloud-init  
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "vnc"
+    listen_type = "address"
+  }
+}
